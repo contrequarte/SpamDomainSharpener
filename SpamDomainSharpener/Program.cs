f3531d044c0d18c1594704e896c807397e6845b4
contrequarte <contrequarte@users.noreply.github.com>
2020-04-11 18:11:07 +0200

    Initial commit of the first version

---
+++ SpamDomainSharpener/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace SpamDomainSharpener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inFile = @"junk_mail.txt";
+            string outFile = @"junk_domain.txt";
+            string outFileFiltered = @"junk_domain_Greater2.txt";
+            List<string> emailList = new List<string>();
+            List<string> domainList = new List<string>();
+            List<string> finalList = new List<string>();
+            emailList.AddRange(File.ReadAllLines(inFile));
+            domainList.AddRange(emailList.Select(e => "@" + e.Split('@')[1]).GroupBy(d => d).Select(g => g.Key + ";" + g.Count().ToString()).OrderBy(r=>r));
+            File.WriteAllLines(outFile, domainList);
+            domainList.Clear();
+            domainList.AddRange(emailList.Select(e => "@" + e.Split('@')[1]).GroupBy(d => d).Where(g=>g.Count()>2).Select(g => g.Key).OrderBy(r=>r));           
+            File.WriteAllLines(outFileFiltered, domainList);
+        }
+    }
+}
